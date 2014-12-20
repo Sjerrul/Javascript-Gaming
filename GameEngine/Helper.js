1ddd847c3a1b7ab9db9311874c433606cb89cfc1
Sjerrul <cerylwiltink@hotmail.com>
2014-12-20 13:38:26 +0100

    Added intersect function

--- GameEngine/Helper.js
+++ GameEngine/Helper.js
@@ -13,5 +13,15 @@ Helper.prototype = {
 
     randomIntInclusive: function (min, max) {
         return Math.floor(this.random(min, max + 1));
+    },
+
+    aabbIntersect: function (ax, ay, aw, ah, bx, by, bw, bh) { //Axis-aligned Bounding Box
+        return (ax < bx + bw) && (ay < by + bh) && (bx < ax + aw) && (by < ay + ah);
     }
+
+    //clamp: function () {
+    //    var x = Math.min(Math.max(a, b), c);
+
+
+    //}
 }
