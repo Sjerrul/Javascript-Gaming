bb5e7cd3e510073dae90b587820c21b836d6797e
Sjerrul <cerylwiltink@hotmail.com>
2015-02-23 16:45:58 +0100

    Created little demo app with a queue between async reader and writer classes

---
+++ Queue/Queue.js
@@ -0,0 +1,31 @@
+﻿function Queue () {
+    this.array = [];
+}
+
+Queue.prototype = {
+    enqueue: function (object) {
+        this.array.push(object);
+    },
+
+    dequeue: function () {
+        if (this.isEmpty()) {
+            throw new Error("Cannot dequeue, queue is empty");
+        }
+        return this.array.shift();
+    },
+
+    peek: function () {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.array[0];
+    },
+
+    isEmpty: function () {
+        return this.array.length === 0;
+    },
+
+    toString: function () {
+        return this.array.join(", ");
+    }
+}
---
+++ Queue/Writer.js
@@ -0,0 +1,24 @@
+﻿function Writer(queue, timeout) {
+    this.timeout = timeout;
+    this.queue = queue;
+    this.busy = false;
+
+    this.runningTasks = 0;
+    this.counter = 0;
+}
+
+Writer.prototype = {
+    write: function () {
+        this.runningTasks++;
+        this.busy = true;
+
+        window.setTimeout(function (me) {
+            me.queue.enqueue(++me.counter);
+            me.runningTasks--;
+
+            if (me.runningTasks <= 0) {
+                me.busy = false;
+            }
+        }, (this.runningTasks + 1) * this.timeout, this);
+    }
+}
